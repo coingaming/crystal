d18240b90bab217f136351329fd19d230e317b38
Ilja Tkachuk <ilja@heathmont.net>
2018-07-13 17:45:40 +0300

    crystal mix task

--- README.md
+++ README.md
@@ -10,11 +10,21 @@ by adding `crystal` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:crystal, "~> 0.1.0"}
+    {:crystal, "~> X.X.X"}
   ]
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/crystal](https://hexdocs.pm/crystal).
+## Usage
+
+Command will check all modules in current mix project
+
+```
+mix crystal
+```
+
+Command will check only given module
+
+```
+mix crystal <ModuleName>
+```
---
+++ lib/mix/tasks/crystal.ex
@@ -0,0 +1,81 @@
+defmodule Mix.Tasks.Crystal do
+  use Mix.Task
+  require Uelli
+
+  @moduledoc """
+  Task runs Gradualizer for current mix project
+  """
+
+  def run([]) do
+    :ok = Mix.Task.run("app.start")
+    Mix.Project.get!.project[:app]
+    |> case do
+      nil ->
+        raise("OTP application name was not provided by mix.exs")
+      otp_app when is_atom(otp_app) ->
+        otp_app
+        |> :application.get_key(:modules)
+        |> case do
+          {:ok, modules = [_ | _]} ->
+            check_modules(modules)
+          :undefined ->
+            raise(":application.get_key(#{inspect otp_app}, :modules) FAILED")
+        end
+    end
+  end
+
+  def run([module_name]) do
+    :ok = Mix.Task.run("app.start")
+    module_name
+    |> String.to_atom
+    |> Crystal.type_check_module
+    |> case do
+      :ok -> :ok
+      :nok -> raise("Gradualizer founded errors in modules: #{module_name}")
+    end
+  end
+
+  #
+  # priv
+  #
+
+  defp check_modules(modules = [_ | _]) do
+    modules
+    |> Stream.map(fn(module_name) ->
+      module_name
+      |> Crystal.type_check_module
+      |> Uelli.try_catch
+      |> case do
+        res = :ok -> {module_name, res}
+        res = :nok -> {module_name, res}
+        {:error, _} -> {module_name, :error}
+      end
+    end)
+    |> Enum.filter(fn
+      {_, :ok} -> false
+      {_, :nok} -> true
+      {_, :error} -> true
+    end)
+    |> case do
+      [] ->
+        :ok
+      bad_modules = [_ | _] ->
+        error_message =
+          bad_modules
+          |> Enum.group_by(fn({_, status}) -> status end)
+          |> Stream.map(fn({status, lst = [_ | _]}) ->
+            prefix =
+              status
+              |> case do
+                :nok -> "Gradualizer founded errors in modules: "
+                :error -> "Gradualizer failed with exception in modules: "
+              end
+            "#{prefix}#{lst |> Stream.map(fn({module_name, _}) -> module_name end) |> Enum.join(", ")}"
+          end)
+          |> Enum.join("\n\n")
+
+        raise("\n\n#{error_message}\n\nTo investigate issues run \"mix crystal <ModuleName>\"\n\n")
+    end
+  end
+
+end
