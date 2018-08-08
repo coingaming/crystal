b8a7b62dd0d082b2c3be617b1350e463f959abb4
Ilja Tkachuk <ilja@heathmont.net>
2018-08-08 13:23:28 +0300

    Elixir 1.7.2 upgrade

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Crystal.MixProject do
       # excoveralls
       test_coverage:      [tool: ExCoveralls],
       preferred_cli_env:  [
-        "coveralls":            :test,
+        coveralls:              :test,
         "coveralls.travis":     :test,
         "coveralls.circle":     :test,
         "coveralls.semaphore":  :test,
