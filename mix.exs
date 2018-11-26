3ab64254ee660ddb4064a65a5b649e3761b68c3b
Ilja Tkachuk <ilja@heathmont.net>
2018-11-26 14:31:43 +0200

    update Uelli

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule Crystal.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:gradualizer, github: "josefs/Gradualizer"},
-      {:uelli, github: "coingaming/uelli"},
+      {:uelli, "~> 0.1.3"},
       # development tools
       {:excoveralls, "~> 0.9", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 0.5",    only: [:dev, :test], runtime: false},
